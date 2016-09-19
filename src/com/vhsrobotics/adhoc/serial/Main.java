9c1efa6d3748ad9e16eb440248cec0771feb516d
J <J@J-PC.attlocal.net>
2016-09-18 23:59:51 -0700

    Adding the SerialFile Data Type

--- src/com/vhsrobotics/adhoc/serial/Main.java
+++ src/com/vhsrobotics/adhoc/serial/Main.java
@@ -3,7 +3,8 @@ package com.vhsrobotics.adhoc.serial;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SerialFile s = new SerialFile();
+		s.setTo(0x455);
 
 	}
 
---
+++ src/com/vhsrobotics/adhoc/serial/SerialFile.java
@@ -0,0 +1,9 @@
+package com.vhsrobotics.adhoc.serial;
+
+public class SerialFile {
+	String value;
+	
+	public void setTo (Object o) {
+		value = o.toString();
+	}
+}
